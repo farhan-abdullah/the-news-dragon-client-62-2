d1264619b169d817bfdfe61a039cd27ae923c2ac
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-04-29 11:17:45 +0200

    created project with jhankar vai

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDe9NH-9IZblJoIEqdTV_c7gSaUMUCt8Nc',
+	authDomain: 'programming-hero-27ece.firebaseapp.com',
+	projectId: 'programming-hero-27ece',
+	storageBucket: 'programming-hero-27ece.appspot.com',
+	messagingSenderId: '321302581752',
+	appId: '1:321302581752:web:d567bb96797a85bca4c60d',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
